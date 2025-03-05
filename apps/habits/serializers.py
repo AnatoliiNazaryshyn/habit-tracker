f17ca07b89eea17cc231f5447c5441f14b868fb2
TohaKorish <tnazaryshyn@gmail.com>
2025-03-05 21:45:33 +0200

    refactor: clean up and simplify serializer method

--- apps/habits/serializers.py
+++ apps/habits/serializers.py
@@ -56,22 +56,25 @@ class HabitLogSerializer(serializers.ModelSerializer):
                 'habit': 'You can only create logs for your own habits.'
             })
 
-        last_log = HabitLog.objects.filter(habit=habit).order_by('-completed_at').first()
+        now = timezone.now()
 
-        if last_log:
-            now = timezone.now()
-            last_log_time = last_log.completed_at
+        if habit.frequency == 'daily':
+            if HabitLog.objects.filter(
+                habit=habit,
+                completed_at__date=now.date()
+            ).exists():
+                raise serializers.ValidationError({
+                    'habit': 'You can only log daily habit once per day.'
+                })
 
-            if habit.frequency == 'daily':
-                if last_log_time.date() == now.date():
-                    raise serializers.ValidationError({
-                        'habit': 'You can only log daily habit once per day.'
-                    })
-
-            elif habit.frequency == 'monthly':
-                if (last_log_time.year == now.year and last_log_time.month == now.month):
-                    raise serializers.ValidationError({
-                        'habit': 'You can only log monthly habit once per month.'
-                    })
+        elif habit.frequency == 'monthly':
+            if HabitLog.objects.filter(
+                habit=habit,
+                completed_at__year=now.year,
+                completed_at__month=now.month
+            ).exists():
+                raise serializers.ValidationError({
+                    'habit': 'You can only log monthly habit once per month.'
+                })
 
         return data
