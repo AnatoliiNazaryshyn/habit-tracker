764ab98f4cfbb84c47cdc9595a367fd5aded53e2
TohaKorish <tnazaryshyn@gmail.com>
2025-03-05 21:05:40 +0200

    feat: create signal to increment current_streak in Goal on HabitLog save

--- apps/habits/apps.py
+++ apps/habits/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class HabitsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.habits"
+
+    def ready(self):
+        import apps.habits.signals  # noqa: F401
