1df4143c467c5708f8d982031a30911b4943a0b3
TohaKorish <tnazaryshyn@gmail.com>
2025-03-05 20:56:50 +0200

    feat: create Habits app with models Habit, HabitLog and Goal

---
+++ apps/habits/constants.py
@@ -0,0 +1,10 @@
+
+FREQUENCY_CHOICES = [
+        ('daily', 'Daily'),
+        ('weekly', 'Weekly'),
+    ]
+
+GOAL_STATUS_CHOICES = [
+    ('in_progress', 'In Progress'),
+    ('completed', 'Completed'),
+]
