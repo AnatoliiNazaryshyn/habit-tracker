e6fea3e1d3328b19a3902bc96a5b3eb2589ff035
TohaKorish <tnazaryshyn@gmail.com>
2025-03-06 23:16:18 +0200

    feat: Add superuser creation command to entrypoint

--- docker/habit_tracker/docker-entrypoint.sh
+++ docker/habit_tracker/docker-entrypoint.sh
@@ -4,6 +4,9 @@
 echo "Applying database migrations..."
 python3 manage.py migrate
 
+# Create superuser if it doesn't exist
+python3 manage.py makesuperuser
+
 # Start the Django server
 echo "Starting Django server..."
 python3 manage.py runserver ${API_HOST}:8000
