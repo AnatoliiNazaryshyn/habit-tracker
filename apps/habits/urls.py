f27714d2fdcd9324aea444818705eae16799751e
TohaKorish <tnazaryshyn@gmail.com>
2025-03-07 22:23:15 +0200

    feat: create reminder endpoint

--- apps/habits/urls.py
+++ apps/habits/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import HabitViewSet, GoalViewSet, HabitLogListCreateView
+from .views import HabitViewSet, GoalViewSet, HabitLogListCreateView, ReminderViewSet
 
 router = DefaultRouter()
 router.register(r'habits', HabitViewSet, basename='habit')
 router.register(r'goals', GoalViewSet, basename='goal')
+router.register(r'reminders', ReminderViewSet, basename='reminder')
 
 app_name = 'habits'
 
